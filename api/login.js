0639f7e429645c15ca8a41d29942ed08247c2b35
Mostafa Baroudi <m.baroudi@esisa.ac.ma>
2026-01-28 15:55:32 +0000

    Parse form body in API

--- api/login.js
+++ api/login.js
@@ -1,5 +1,27 @@
 const bcrypt = require("bcryptjs");
 const { getPool } = require("./_db");
+const querystring = require("querystring");
+
+function readBody(req) {
+  return new Promise((resolve) => {
+    let data = "";
+    req.on("data", (chunk) => {
+      data += chunk;
+    });
+    req.on("end", () => {
+      const contentType = req.headers["content-type"] || "";
+      if (contentType.includes("application/json")) {
+        try {
+          resolve(JSON.parse(data || "{}"));
+        } catch {
+          resolve({});
+        }
+        return;
+      }
+      resolve(querystring.parse(data));
+    });
+  });
+}
 
 module.exports = async (req, res) => {
   if (req.method !== "POST") {
@@ -8,7 +30,8 @@ module.exports = async (req, res) => {
     return;
   }
 
-  const { email, password } = req.body || {};
+  const body = req.body || (await readBody(req));
+  const { email, password } = body;
 
   if (!email || !password) {
     res.writeHead(302, { Location: "/login.html" });
